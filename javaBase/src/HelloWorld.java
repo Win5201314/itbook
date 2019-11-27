050c551f9462e21daf4b155430c7cb71d42b01ec
Win5201314 <zsl>
2019-11-27 14:51:48 +0800

    httpclient

---
+++ javaBase/src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    //main方法详解：https://www.cnblogs.com/dodocie/p/7439651.html
+    public static void main(String[] args) {
+        Integer i = 5;
+        System.out.println(i.intValue());
+        Character c = new Character('5');
+        System.out.println(Character.isDigit('5'));
+    }
+    //难点
+    //1.java修饰符
+    //2.java 面向对象知识
+    //3.
+    //
+    //
+}
